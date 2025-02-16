87493afccb7ce0b7da1bf4a205d63380f09f2ee4
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-02-16 17:55:39 +0530

    🚀 Improved bidController.js - Enhanced bid placement logic for better validation and error handling

--- BACKEND/controllers/bidController.js
+++ BACKEND/controllers/bidController.js
@@ -25,47 +25,51 @@ export const placeBid = catchAsyncErrors(async (req, res, next) => {
     );
   }
 
-//   try {
-//     const existingBid = await Bid.findOne({
-//       "bidder.id": req.user._id,
-//       auctionItem: auctionItem._id,
-//     });
-//     const existingBidInAuction = auctionItem.bids.find(
-//       (bid) => bid.userId.toString() == req.user._id.toString()
-//     );
-//     if (existingBid && existingBidInAuction) {
-//       existingBidInAuction.amount = amount;
-//       existingBid.amount = amount;
-//       await existingBidInAuction.save();
-//       await existingBid.save();
-//       auctionItem.currentBid = amount;
-//     } else {
-//       const bidderDetail = await User.findById(req.user._id);
-//       const bid = await Bid.create({
-//         amount,
-//         bidder: {
-//           id: bidderDetail._id,
-//           userName: bidderDetail.userName,
-//           profileImage: bidderDetail.profileImage?.url,
-//         },
-//         auctionItem: auctionItem._id,
-//       });
-//       auctionItem.bids.push({
-//         userId: req.user._id,
-//         userName: bidderDetail.userName,
-//         profileImage: bidderDetail.profileImage?.url,
-//         amount,
-//       });
-//       auctionItem.currentBid = amount;
-//     }
-//     await auctionItem.save();
+  try {
+    const existingBid = await Bid.findOne({
+      "bidder.id": req.user._id,
+      auctionItem: auctionItem._id,
+    });
+    const existingBidInAuction = auctionItem.bids.find(
+      (bid) => bid.userId.toString() == req.user._id.toString()
+    );
+    if (existingBid && existingBidInAuction) {
+      existingBidInAuction.amount = amount;
+      existingBid.amount = amount;
+      await existingBidInAuction.save();
+      await existingBid.save();
+      auctionItem.currentBid = amount;
+    } else {
+      const bidderDetail = await User.findById(req.user._id);
+      const bid = await Bid.create({
+        amount,
+        bidder: {
+          id: bidderDetail._id,
+          userName: bidderDetail.userName,
+          profileImage: bidderDetail.profileImage?.url,
+        },
+        auctionItem: auctionItem._id,
+      });
+      auctionItem.bids.push({
+        userId: req.user._id,
+        userName: bidderDetail.userName,
+        profileImage: bidderDetail.profileImage?.url,
+        amount,
+      });
+      auctionItem.currentBid = amount;
+    }
+    await auctionItem.save();
 
-//     res.status(201).json({
-//       success: true,
-//       message: "Bid placed.",
-//       currentBid: auctionItem.currentBid,
-//     });
-//   } catch (error) {
-//     return next(new ErrorHandler(error.message || "Failed to place bid.", 500));
-//   }
-});
+    res.status(201).json({
+      success: true,
+      message: "Bid placed.",
+      currentBid: auctionItem.currentBid,
+    });
+  } catch (error) {
+    return next(new ErrorHandler(error.message || "Failed to place bid.", 500));
+  }
+});   
+
+
+
+ 
