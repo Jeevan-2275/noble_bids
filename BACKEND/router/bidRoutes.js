349c0b8fe818dc16000a1fce133f37fb57112d41
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-02-16 19:41:09 +0530

    🔄 Updated bidRoutes.js - Integrated checkAuctionEndTime middleware

--- BACKEND/router/bidRoutes.js
+++ BACKEND/router/bidRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { placeBid } from "../controllers/bidController.js";
 import { isAuthenticated, isAuthorized } from "../middlewares/auth.js";
-// import { checkAuctionEndTime } from "../middlewares/checkAuctionEndTime.js";
+import { checkAuctionEndTime } from "../middlewares/checkAuctionEndTime.js";
 
 const router = express.Router();
 
@@ -9,7 +9,7 @@ router.post(
   "/place/:id",
   isAuthenticated,
   isAuthorized("Bidder"),
-//   checkAuctionEndTime,
+  checkAuctionEndTime,
   placeBid
 );
 
