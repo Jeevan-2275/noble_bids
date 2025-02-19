bea41c4ef4dc5c2420d9d6d6431d8f70b0b1882a
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-02-19 22:02:16 +0530

    Added About component import in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,6 +10,7 @@ import SubmitCommission from "./pages/SubmitCommission";
 import {  fetchUser } from "./store/slices/userSlice";
 import { useDispatch } from "react-redux";
 import HowItWorks from "./pages/HowItWorks";
+import About from "./pages/About";
 
 
 
@@ -30,6 +31,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/submit-commission" element={<SubmitCommission />} />
         <Route path="/how-it-works-info" element={<HowItWorks />} />
+        <Route path="/about" element={<About />} />
 
       </Routes>
       <ToastContainer position="top-right" />
