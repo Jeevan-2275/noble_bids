16a89da1192e740061ad6842c00db9dc892c452a
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-02-19 11:04:56 +0530

    Fix import path for commissionReducer in store.js

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
+import commissionReducer from "./slices/commissionSlice";
 
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    commission:commissionReducer
 
   },
 });
