0a7142cdff84e6bcb71939cabd8134bfa0d0cc85
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-02-22 12:39:33 +0530

    Refactor: Rename userName to username in auctionItemController.js for consistency

--- BACKEND/controllers/auctionItemController.js
+++ BACKEND/controllers/auctionItemController.js
@@ -120,7 +120,7 @@ export const getAuctionDetails = catchAsyncErrors(async (req, res, next) => {
   if (!auctionItem) {
     return next(new ErrorHandler("Auction not found.", 404));
   }
-  const bidders = auctionItem.bids.sort((a, b) => b.bid - a.bid);
+  const bidders = auctionItem.bids.sort((a, b) => b.amount - a.amount);
   res.status(200).json({
     success: true,
     auctionItem,
