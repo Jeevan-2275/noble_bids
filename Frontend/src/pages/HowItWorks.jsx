0c933c45162f36f3978ff572dcc44ca659a7886a
Jeevan kadam$ <jeevankadam2275@gmail.com>
2025-03-05 13:57:12 +0530

    Updated HowItWorks page

--- Frontend/src/pages/HowItWorks.jsx
+++ Frontend/src/pages/HowItWorks.jsx
@@ -53,7 +53,7 @@ const HowItWorks = () => {
       <div className="max-w-6xl mx-auto py-12">
         <div className="text-center mb-16">
           <h1 className="text-4xl md:text-6xl font-bold text-[#d6482b] mb-4 relative pb-4">
-            How PrimeBid Operates
+            How NobleBid Operates
             <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-24 h-1.5 bg-[#d6482b] rounded-full"></span>
           </h1>
           <p className="text-xl text-gray-600 max-w-2xl mx-auto">
